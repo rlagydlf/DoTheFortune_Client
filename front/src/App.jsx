b2d6e624d4cdccf13a7016f17827dcbd79e9a6e1
elroi773 <s2457@e-mirim.hs.kr>
2025-12-31 18:58:47 +0900

    라우터 추가

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -1,10 +1,13 @@
-import { Routes, Route, Navigate } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import Login from "./pages/Auth/Login";
+import SignUp from "./pages/Auth/SignUp";
 
 export default function App() {
   return (
-    <Routes>
-      <Route path="/login" element={<Login />} />
-    </Routes>
+      <Routes>
+        <Route path="/" element={<Navigate to="/login" replace />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<SignUp />} />
+      </Routes>
   );
 }
