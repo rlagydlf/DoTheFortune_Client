1418cb7f1194c68477c8eef1b9ebfa8feb2be039
elroi773 <s2457@e-mirim.hs.kr>
2025-12-31 18:08:21 +0900

    기본 세팅 및 router 세팅 포함

---
+++ front/src/App.jsx
@@ -0,0 +1,10 @@
+import { Routes, Route, Navigate } from "react-router-dom";
+import Login from "./pages/Auth/Login";
+
+export default function App() {
+  return (
+    <Routes>
+      <Route path="/login" element={<Login />} />
+    </Routes>
+  );
+}
